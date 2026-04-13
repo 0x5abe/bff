bf18c2c00f70b74d03d034145ccd856e66418254
widberg <widberg@widberg.me>
2026-04-13 17:48:49 -0400

    Remove generics

--- bff/src/macros/classes.rs
+++ bff/src/macros/classes.rs
@@ -1,7 +1,6 @@
 macro_rules! classes {
     (
         $(
-            $(#![$generic:ident])?
             $class:ident
             $({
                 $($pattern:pat => $variant_mod:ident::$variant:ident),+ $(,)?
@@ -12,7 +11,6 @@ macro_rules! classes {
     ) => {
         $(
             $crate::macros::classes::classes!(@entry
-                $(#![$generic])?
                 $class
                 $({
                     $($pattern => $variant_mod::$variant),+
@@ -159,17 +157,11 @@ macro_rules! classes {
         }
     };
 
-    (@entry #![generic] $class:ident { $($body:tt)* }) => {
-        $crate::macros::classes::classes!(@module generic $class { $($body)* });
-    };
-    (@entry #![generic] $class:ident) => {
-        $crate::macros::classes::classes!(@module generic $class {});
-    };
     (@entry $class:ident { $($body:tt)* }) => {
-        $crate::macros::classes::classes!(@module plain $class { $($body)* });
+        $crate::macros::classes::classes!(@module $class { $($body)* });
     };
     (@entry $class:ident) => {
-        $crate::macros::classes::classes!(@module plain $class {});
+        $crate::macros::classes::classes!(@module $class {});
     };
 
     (@class_ty $class:ident) => {
@@ -194,23 +186,16 @@ macro_rules! classes {
         }
     }};
 
-    (@module_kind_prelude generic) => {
-        pub mod generic;
-    };
-    (@module_kind_prelude plain) => {};
-
-    (@module $kind:ident $class:ident {}) => {
+    (@module $class:ident {}) => {
         pastey::paste! {
             pub mod [<#$class:snake>] {
-                $crate::macros::classes::classes!(@module_kind_prelude $kind);
-                $crate::macros::classes::classes!(@declare_class_kind $kind $class {});
+                $crate::macros::classes::classes!(@declare_class $class {});
             }
         }
     };
-    (@module $kind:ident $class:ident { $($pattern:pat => $variant_mod:ident::$variant:ident),+ $(,)? $(; $(pub mod $extra_mod:ident;)*)? }) => {
+    (@module $class:ident { $($pattern:pat => $variant_mod:ident::$variant:ident),+ $(,)? $(; $(pub mod $extra_mod:ident;)*)? }) => {
         pastey::paste! {
             pub mod [<#$class:snake>] {
-                $crate::macros::classes::classes!(@module_kind_prelude $kind);
                 $(
                     $(
                         pub mod $extra_mod;
@@ -220,16 +205,9 @@ macro_rules! classes {
                     pub mod $variant_mod;
                     use self::$variant_mod::$variant;
                 )*
-                $crate::macros::classes::classes!(@declare_class_kind $kind $class { $($pattern => $variant),* });
+                $crate::macros::classes::classes!(@declare_class $class { $($pattern => $variant),* });
             }
         }
-    };
-
-    (@declare_class_kind generic $class:ident $variants:tt) => {
-        $crate::macros::classes::classes!(@declare_class #![generic] $class $variants);
-    };
-    (@declare_class_kind plain $class:ident $variants:tt) => {
-        $crate::macros::classes::classes!(@declare_class $class $variants);
     };
 
     (@named_class $class:ident) => {
@@ -441,25 +419,6 @@ macro_rules! classes {
                         writeln!(f, "{}: {}", stringify!($variant), self.$variant)?;
                     )*
                     Ok(())
-                }
-            }
-        }
-    };
-
-    (@declare_class #![generic] $class:ident { $($pattern:pat => $variant:ident),* $(,)? }) => {
-        $crate::macros::classes::classes!(@declare_class $class { $($pattern => $variant),* });
-        $crate::macros::classes::classes!(@declare_class_generic_from $class { $($pattern => $variant),* });
-    };
-
-    (@declare_class_generic_from $class:ident { $($pattern:pat => $variant:ident),* $(,)? }) => {
-        pastey::paste! {
-            impl From<$class> for generic::[<$class Generic>] {
-                fn from(class: $class) -> generic::[<$class Generic>] {
-                    match class {
-                        $($class::$variant(class) => {
-                            (*class).into()
-                        })*
-                    }
                 }
             }
         }
